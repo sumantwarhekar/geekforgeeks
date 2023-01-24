673321dcc864d88217ae4c097e0a22ec21ff86c5
Sumant Warhekar <sumantwarhekar@gmail.com>
2023-01-24 22:54:22 +0530

    Updated 'Check if two arrays are equal or not'

--- basic/Check if two arrays are equal or not.java
+++ basic/Check if two arrays are equal or not.java
@@ -1,12 +1,13 @@
-class Solution{
-    //Function to check if two arrays are equal or not.
-    public static boolean check(long A[],long B[],int N)
-    {
-        //Your code here
+import java.util.*;
+
+class Solution {
+    // Function to check if two arrays are equal or not.
+    public static boolean check(long A[], long B[], int N) {
+        // Your code here
         Arrays.sort(A);
         Arrays.sort(B);
-        for(int i=0;i<N;i++){
-            if(A[i]!=B[i])
+        for (int i = 0; i < N; i++) {
+            if (A[i] != B[i])
                 return false;
         }
         return true;
