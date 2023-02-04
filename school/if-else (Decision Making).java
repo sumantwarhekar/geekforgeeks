b0e48e29fc54dd11beff604dbd81e9065b9a34fb
Sumant Warhekar <sumantwarhekar@gmail.com>
2023-02-04 23:00:10 +0530

    Progress on 4th Feb 2023

---
+++ school/if-else (Decision Making).java
@@ -0,0 +1,11 @@
+class Solution {
+    static String compareNM(int n, int m) {
+        // code here
+        if (n < m)
+            return "lesser";
+        else if (n == m)
+            return "equal";
+        else
+            return "greater";
+    }
+}
