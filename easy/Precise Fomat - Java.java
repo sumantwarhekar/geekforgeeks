691c177548d84f35570479c6d90928887750e33f
Sumant Warhekar <sumantwarhekar@gmail.com>
2023-02-08 14:12:51 +0530

    Progress on 8th Feb 2023

---
+++ easy/Precise Fomat - Java.java
@@ -0,0 +1,11 @@
+class Geeks{
+    
+    static void printInFormat(float a, float b){
+        float result = a/b;
+        System.out.print(result + " ");
+        System.out.printf("%.3f",result);
+        // Your code here to print upto 3 decimal places
+        
+    }    
+    
+}
