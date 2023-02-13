da5742a78be6b5ffc13e73862f7e0875ab92f68a
Sumant Warhekar <sumantwarhekar@gmail.com>
2023-02-13 16:31:44 +0530

    Progress on 13th Feb 2023

---
+++ school/Multiplication Table.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    static ArrayList<Integer> getTable(int N) {
+        // code here
+        ArrayList<Integer> table = new ArrayList<Integer>();
+        for (int i = 1; i <= 10; i++) {
+            table.add(N * i);
+        }
+        return table;
+    }
+}
