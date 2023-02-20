98d1fe39b1a88d5dca9fd0400c809c2bad25c4cc
Sumant Warhekar <sumantwarhekar@gmail.com>
2023-02-20 23:46:42 +0530

    Progress on 20th Feb 2023

---
+++ school/Learn to Comment - Java.java
@@ -0,0 +1,18 @@
+class Geeks {
+
+    // Function to print a and c
+    // comment the line that outputs b
+    static void comment(int a, int b, int c) {
+        // This a single line comment
+        /*
+         * Multi line comment
+         */
+        // Prints a
+        System.out.println(a);
+        // Prints b
+
+        // Prints c
+        System.out.println(c);
+    }
+
+}
