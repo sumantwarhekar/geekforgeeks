e56ef8d0828ac1544b41c7e1ff27d667e7a13800
Sumant Warhekar <sumantwarhekar@gmail.com>
2023-02-25 23:24:43 +0530

    Progress on 25th Feb 2023

---
+++ school/Swap two numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    static List<Integer> get(int a, int b) {
+        List<Integer> answer = new ArrayList<Integer>();
+        answer.add(b);
+        answer.add(a);
+        return answer;
+    }
+}
